1f3f08516ae48cf9641d4e771c75b5e9c372e61f
Procoder16 <smsoumik1608@gmail.com>
2021-12-15 01:28:39 +0530

    project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'dart:async';
+import 'dart:math' as math;
 
 void main() {
-  return runApp(_ChartApp());
+  runApp(MyApp());
 }
 
-class _ChartApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _MyHomePage(),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class _MyHomePage extends StatefulWidget {
-  // ignore: prefer_const_constructors_in_immutables
-  _MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<_MyHomePage> {
-  late List<_ChartData> data;
-  late List<_ChartData> data2;
-  late TooltipBehavior _tooltip;
+class _MyHomePageState extends State<MyHomePage> {
+  late List<LiveData> chartData;
+  late List<LiveData> chartData2;
+  late ChartSeriesController _chartSeriesController;
+  late ChartSeriesController _chartSeriesController2;
 
   @override
   void initState() {
-    data = [
-      _ChartData(1, 12),
-      _ChartData(2, 15),
-      _ChartData(3, 30),
-      _ChartData(4, 6.4),
-      _ChartData(5, 14)
-    ];
-    data2 = [
-      _ChartData(1, 24),
-      _ChartData(2, 20),
-      _ChartData(3, 10),
-      _ChartData(4, 11.4),
-      _ChartData(5, 19),
-    ];
-    _tooltip = TooltipBehavior(enable: true);
+    chartData = getChartData();
+    chartData2 = getChartData2();
+    Timer.periodic(const Duration(seconds: 1), updateDataSource);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Syncfusion Flutter chart'),
-      ),
-      body: SfCartesianChart(
-        plotAreaBorderWidth: 0,
-        primaryXAxis: NumericAxis(
-          majorGridLines: MajorGridLines(width: 0),
-          axisLine: AxisLine(width: 0),
-          isVisible: false,
-        ),
-        primaryYAxis: NumericAxis(
-          isVisible: false,
-          minimum: 0,
-          maximum: 50,
-          interval: 10,
-          majorGridLines: MajorGridLines(width: 0),
-          axisLine: AxisLine(width: 0),
-        ),
-        tooltipBehavior: _tooltip,
-        series: <ChartSeries<_ChartData, double>>[
-          SplineAreaSeries<_ChartData, double>(
-            dataSource: data,
-            xValueMapper: (_ChartData data, _) => data.x,
-            yValueMapper: (_ChartData data, _) => data.y,
-            name: 'Gold',
-            color: Colors.blue.withOpacity(0.7),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          //color: Colors.black,
+          height: 200,
+          child: SfCartesianChart(
+            backgroundColor: Colors.black,
+            plotAreaBorderWidth: 0,
+            margin: EdgeInsets.all(0),
+            series: <ChartSeries<LiveData, int>>[
+              SplineAreaSeries<LiveData, int>(
+                borderColor: Colors.red,
+                borderWidth: 2,
+                onRendererCreated: (ChartSeriesController controller) {
+                  _chartSeriesController = controller;
+                },
+                dataSource: chartData,
+                color: Colors.red.withOpacity(0.5),
+                xValueMapper: (LiveData sales, _) => sales.time,
+                yValueMapper: (LiveData sales, _) => sales.speed,
+              ),
+              SplineAreaSeries<LiveData, int>(
+                borderColor: Colors.blue,
+                borderWidth: 2,
+                onRendererCreated: (ChartSeriesController controller) {
+                  _chartSeriesController2 = controller;
+                },
+                dataSource: chartData2,
+                color: Colors.blue.withOpacity(0.5),
+                xValueMapper: (LiveData sales, _) => sales.time,
+                yValueMapper: (LiveData sales, _) => sales.speed,
+              ),
+            ],
+            primaryXAxis: NumericAxis(
+              isVisible: false,
+              majorGridLines: const MajorGridLines(width: 0),
+              axisLine: AxisLine(width: 0),
+              edgeLabelPlacement: EdgeLabelPlacement.shift,
+              interval: 3,
+            ),
+            primaryYAxis: NumericAxis(
+              isVisible: false,
+              majorGridLines: MajorGridLines(width: 0),
+              axisLine: const AxisLine(width: 0),
+              majorTickLines: const MajorTickLines(size: 0),
+            ),
           ),
-          SplineAreaSeries<_ChartData, double>(
-            dataSource: data2,
-            xValueMapper: (_ChartData data, _) => data.x,
-            yValueMapper: (_ChartData data, _) => data.y,
-            name: 'Gold',
-            color: Colors.red.withOpacity(0.7),
-          ),
-        ],
+        ),
       ),
     );
   }
+
+  int time = 10;
+  void updateDataSource(Timer timer) {
+    chartData.add(LiveData(time++, (math.Random().nextInt(60) + 30)));
+    chartData.removeAt(0);
+    _chartSeriesController.updateDataSource(
+      addedDataIndex: chartData.length - 1,
+      removedDataIndex: 0,
+    );
+    chartData2.add(LiveData(time++, (math.Random().nextInt(30) + 10)));
+    chartData2.removeAt(0);
+    _chartSeriesController2.updateDataSource(
+      addedDataIndex: chartData2.length - 1,
+      removedDataIndex: 0,
+    );
+  }
+
+  List<LiveData> getChartData() {
+    return <LiveData>[
+      LiveData(0, 0),
+      LiveData(1, 0),
+      LiveData(2, 0),
+      LiveData(3, 0),
+      LiveData(4, 0),
+      LiveData(5, 0),
+      LiveData(6, 0),
+      LiveData(7, 0),
+      LiveData(8, 0),
+      LiveData(9, 0),
+    ];
+  }
+
+  List<LiveData> getChartData2() {
+    return <LiveData>[
+      LiveData(0, 0),
+      LiveData(1, 0),
+      LiveData(2, 0),
+      LiveData(3, 0),
+      LiveData(4, 0),
+      LiveData(5, 0),
+      LiveData(6, 0),
+      LiveData(7, 0),
+      LiveData(8, 0),
+      LiveData(9, 0),
+    ];
+  }
 }
 
-class _ChartData {
-  _ChartData(this.x, this.y);
-
-  final double x;
-  final double y;
+class LiveData {
+  LiveData(this.time, this.speed);
+  final int time;
+  final num speed;
 }
